54e4acea2735b6eb2622f84c9177c7ebb415cc2a
Ridwan <ridwanpr@outlook.com>
2025-02-18 10:59:57 +0700

    refactor fetch all manga

--- app/Jobs/FetchAllMangaJob.php
+++ app/Jobs/FetchAllMangaJob.php
@@ -25,8 +25,15 @@ class FetchAllMangaJob implements ShouldQueue
     public function handle(): void
     {
         try {
+            $userAgents = [
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36',
+                'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36'
+            ];
+            $randomUserAgent = $userAgents[array_rand($userAgents)];
+
             $response = Http::withHeaders([
-                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+                'User-Agent' => $randomUserAgent
             ])->get('https://manhwaindo.one/series/list-mode/');
 
             if ($response->successful()) {
