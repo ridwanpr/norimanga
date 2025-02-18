7e252d06ebd6c35574494aaa39c7a84eb39e43f0
Ridwan <ridwanpr@outlook.com>
2025-02-18 13:05:35 +0700

    fix random user agent

--- app/Jobs/FetchAllMangaJob.php
+++ app/Jobs/FetchAllMangaJob.php
@@ -26,9 +26,8 @@ class FetchAllMangaJob implements ShouldQueue
     {
         try {
             $userAgents = [
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
-                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36',
-                'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36'
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36',
             ];
             $randomUserAgent = $userAgents[array_rand($userAgents)];
 
